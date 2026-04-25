890758fb2a2a9d521919b19518015fec5e42040a
atiullah-mohd <atiullah7786@gmail>
2026-04-25 14:41:16 +0530

    code commit

--- backendnodejs/nodefirst/src/controller/userController.js
+++ backendnodejs/nodefirst/src/controller/userController.js
@@ -5,7 +5,7 @@ import pool from "../config/db.js";
 const handleResponse=(res, status,message,data=null)=>{
     res.status(status).json({status, message, data });
 };
-
+//good
 export const createUser=async(req,res,next)=>{
     const userData=req.body;
 
